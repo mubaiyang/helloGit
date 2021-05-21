f34f2bb9db5a75a12d569767650635035f2def8b
mubaiyang <1938865925@qq.com>
2021-05-21 17:42:20 +0800

    更新2

--- githeloo/src/main/java/com/hellogit.java
+++ githeloo/src/main/java/com/hellogit.java
@@ -4,5 +4,6 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("更新一");
+        System.out.println("更新2");
     }
 }
